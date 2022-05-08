31bf5a16a2137c73ee6ecb1f5804490252dca6ba
smilejakdu <ash982416@gmail.com>
2022-05-08 19:36:07 +0900

    travis file update2

--- src/App.test.js
+++ src/App.test.js
@@ -12,3 +12,9 @@ test('renders learn react link2', () => {
   const linkElement = screen.getByText(/learn react/i)
   expect(linkElement).toBeInTheDocument()
 })
+
+test('renders learn react link3', () => {
+  render(<App />)
+  const linkElement = screen.getByText(/learn react/i)
+  expect(linkElement).toBeInTheDocument()
+})
